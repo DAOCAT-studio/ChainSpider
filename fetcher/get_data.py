23c6f6843c0497ca529890ae98460d5fbf7bd941
onlyhwt <1659026978@qq.com>
2022-07-13 09:16:15 +0800

    更新查询实例

--- fetcher/get_data.py
+++ fetcher/get_data.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 # 独立于其他脚本以外的查询实例
-def se_data(t, symbol):
+def se_data(symbol):
     try:
         host = '43.158.211.160'
         port = 3306
@@ -12,7 +12,7 @@ def se_data(t, symbol):
 
         conn = pymysql.connect(host=host, user=user, password=passwd, database=db, port=port,
                                autocommit=True)
-        t_symbol = "{}_{}".format(t,symbol)
+
         with conn:
             with conn.cursor() as cursor:
                 # sql = "SELECT * FROM glassnode WHERE t_symbol=%s "
@@ -27,4 +27,4 @@ def se_data(t, symbol):
 
 
 if __name__ == '__main__':
-    print(se_data('1604966400', 'BTC'))
+    print(se_data('BTC'))
