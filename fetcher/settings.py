565ef7cb31ee866c90ea82f76a1686e349db7e6c
onlyhwt <1659026978@qq.com>
2022-07-18 11:23:08 +0800

    fix config

--- fetcher/settings.py
+++ fetcher/settings.py
@@ -4,15 +4,16 @@ import sys
 API_KEY_LIST = ['2BPofIMYS789jEMXbi8dcEiF4Xj', '2BF0rMZPoEZzyDP0v913XREBCsO', '2BY58n2bpVtRxoAGgph2UThtyQa',
                 '2BmTVlIqRKYCPrKVdznuO5oFdHq', '2BmTvkJL6zDjTQZpS4xonOKqmVE', '2BmUM2A7xjKlLCqhh8f8HUgDLwC']
 
-cf = configparser.ConfigParser()
-cf.read(r'./config.ini')
-
 if sys.platform == 'win32':
     # local
+    cf = configparser.ConfigParser()
+    cf.read('config.ini')
     db_set = cf['local']
 
 else:
     # server
+    cf = configparser.ConfigParser()
+    cf.read('config.ini')
     db_set = cf['server']
 
 HOST = db_set['HOST']
